e3eacd532e997312878959cada9eeb2adc265fb7
bernardo-zuchowski <ber.s.zuc@gmail.com>
2021-11-06 18:25:37 -0300

    !merge: responsive to mocking

---
+++ components/user-profile/NewTweet/Form.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <input
+        class="mt-4 w-full shadow-inner rounded-lg bg-gray-100 py-1 px-2 text-black"
+        type="text"
+        placeholder="Compose new Tweet"
+        >
+    </div>
+</template>
