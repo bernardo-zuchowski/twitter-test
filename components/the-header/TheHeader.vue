ebc37880ba688421138613e3e32ee84eeda816d4
bernardo-zuchowski <ber.s.zuc@gmail.com>
2021-11-07 21:36:53 -0300

    README.MD

--- components/the-header/TheHeader.vue
+++ components/the-header/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="shadow-lg bg-white border-b  p-4">
-        <Sidebar v-if="toggle" :class="{'focus:':toggle}" class="md-min:hidden"/>
-        <nav class="flex justify-between items-center">
+    <header class="shadow-lg bg-white border-b p-4">
+        <Sidebar v-if="toggle" class="md-min:hidden"/>
+        <nav class="max-w-7xl mx-auto flex justify-between items-center">
             <button @click="toggle = !toggle" class="md-min:hidden">
                 <SolidXIcon v-if="toggle" class="ml-1 w-8 h-8 shadow-md hover:shadow-inner bg-gray-50 rounded-full"/>
                 <SolidMenuIcon v-else class="ml-1 w-8 h-8"/>
