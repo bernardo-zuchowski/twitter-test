cc49c85c2e17810dd25f7180fa525c31055f93df
bernardo-zuchowski <ber.s.zuc@gmail.com>
2021-11-15 13:29:34 -0300

    chore: add vuex

---
+++ store/index.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+    user: [],
+    users: [],
+    tweets: [],
+    favorites: []
+})
+
+export const mutations =  {
+    SET_LOGGED_USER(state, user) {
+        state.user = user
+    },
+    SET_USERS(state, users) {
+        state.users = users
+    },
+    SET_TWEETS(state, tweets) {
+        state.tweets = tweets
+    },
+    SET_FAVORITES(state, favorites) {
+        state.favorites = favorites
+    },
+}
+
+export const actions =  {
+    async getLoggedUser({ commit }) {
+        const route = await this.$axios.get('users/1')
+        commit('SET_LOGGED_USER', route.data.user)
+    },
+    async getUsers({ commit }) {
+        const route = await this.$axios.get('users')
+        commit('SET_LOGGED_USER', route.data.users)
+    },
+    async getTweets({ commit }) {
+        const route = await this.$axios.get('tweets')
+        commit('SET_LOGGED_USER', route.data.tweets)
+    },
+}
