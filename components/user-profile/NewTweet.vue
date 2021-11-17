8abcd3d0acaff789a69c36ee2460c63c8da8ea3e
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2021-11-17 16:52:20 -0300

    chore: submit buttons and keyboard event listener

--- components/user-profile/NewTweet.vue
+++ components/user-profile/NewTweet.vue
@@ -1,5 +1,8 @@
 <template>
-    <form ref="newTweet" @submit.prevent="createTweet">
+    <form
+    ref="newTweet"
+    @keyup.shift.enter.prevent="createTweet"
+    class="relative">
         <textarea
         rows="2"
         class="lg-min:mt-6 lg-max:mt-4 resize-none w-full shadow-inner rounded-xl bg-gray-100 py-1 px-2 text-black"
@@ -7,6 +10,14 @@
         placeholder="Compose new tweet"
         v-model="text"
         />
+        <div class="text-xs absolute bottom-4 right-14 bg-gradient-to-t from-gray-50 to-gray-100 pr-3 pl-2">
+            shift+enter or
+        </div>
+        <button
+        @click.prevent="createTweet"
+        class="absolute text-xs right-8 bottom-3 bg-gray-50 rounded-full shadow hover:shadow-inner hover:bg-gray-100">
+            <SolidCheckIcon class="w-5 h-5 m-1"/>
+        </button>
     </form>
 </template>
 
